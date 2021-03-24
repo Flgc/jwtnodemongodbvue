09b151604f76bede0f4ba19c28fd15c04e2caf7c
Fabio Luis <ml@fabiotecnico1.biz>
2021-03-24 00:54:25 -0300

    Rota default da aplicacao implementada

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+/**
+ * file: server.js
+ * Description: Responsável por configurar toda a execução da API
+ * Data: 23/03/2021
+ */
+
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+   console.log('Aplicação em execução na porta...:', port);
+});
---
+++ backend/src/routes/index.js
@@ -0,0 +1,24 @@
+/**
+ * file: app.js
+ * Description: Responsável pela rota default da API
+ * Data: 23/03/2021
+ */
+
+
+ const express = require('express');
+
+ const router = express.Router();
+
+ // ==> v1 = 1ª versao da API:
+ router.get('/api/v1', (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: 'Seja bem-vindo(a) a API Node.js + MongoDb',
+    version: '1.0.0',
+  })
+ });
+
+ module.exports = router;
+
+
+
