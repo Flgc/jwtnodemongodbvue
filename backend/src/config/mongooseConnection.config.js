061d9362321ef12d4f6f13fb1d5d4525e8d38ce1
Fabio Luis <ml@fabiotecnico1.biz>
2021-03-27 14:16:23 -0300

    Refaturando app.js - desacoplando conexao com banco de dados (mongooseConnection.config)  - clean cod - tdd

--- backend/src/config/mongooseConnection.config.js
+++ backend/src/config/mongooseConnection.config.js
@@ -1,5 +1,30 @@
 /**
  * file: src/config/mongooseConnection.config.js
- * Description: 
- * Data: 
+ * Description: Responsável pela conexão através do mongoose
+ * Data: 27/03/2021
  */
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+// ==> Importando o arquivo: 'db.config.js'
+const database = require("./db.config"); //Conexão loca da base de dados
+
+mongoose.Promise = global.Promise;
+
+// ==> Conectando a base de dados
+mongoose
+  .connect(database.local.localDatabaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    () => {
+      console.log("Base de dados conectada com sucesso!");
+    },
+    (err) => {
+      console.log(`Erro ao conectar com a Base de dados..: ${err}`);
+      process.exit();
+    }
+  );
